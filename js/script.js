6c7317428a1e43738e575425676877c5aa8965b0
steve1985 <stephan.che@gmail.com>
2024-02-17 19:42:21 +0400

    fixing music

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const scoreDisplay = document.querySelector('[data-score-display]');
 const highestScoreDisplay = document.querySelector('[data-highest-score]');
 const deathSound = new Audio('./Sounds/Mario Death  Sound Effect HD.mp3');
 const milestoneSound = new Audio('./Sounds/Mario Bros Life up Sound Effect.mp3');
-const jumpSound = new Audio('../Sounds/Mario Jump Sound Effect.mp3');
+const jumpSound = new Audio('./Sounds/Mario Jump Sound Effect.mp3');
 const gameOverSound = new Audio('./Sounds/Mario Death  Sound Effect HD.mp3');
 
 setPixelToWorldScale();
